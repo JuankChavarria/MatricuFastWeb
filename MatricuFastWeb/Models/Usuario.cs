4ac052af6c09a0fb2c50565b333e4141c1baa98d
Juan Carlos Nuñez Chavarria <134462352+JuankChavarria@users.noreply.github.com>
2024-10-06 21:32:35 -0600

    Creacion de proyecto

---
+++ MatricuFastWeb/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatricuFastWeb.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set;}
+        public string Constraseña { get; set; }
+        public string Email { get; set; }
+        public string AdicionadoPor { get; set; }
+        public string FechaAdicion { get; set; }
+        public string ModificadoPor { get; set; }
+        public string FechaModificacion { get; set; }
+
+  
+
+
+    }
+}
